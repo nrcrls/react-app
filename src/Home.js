97c4fed0911b8eea7aa726e972f92710c57429b7
Nikki Carlos <nikkirosecarlos@gmail.com>
2022-09-15 13:54:06 +0900

    created a reusable component for blog posts

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Bloglist from './Bloglist';
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
@@ -9,13 +10,7 @@ const Home = () => {
 
   return (
     <div className="home">
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{ blog.title }</h2>
-          <p>Written by { blog.author }</p>
-        </div>
-      ))}
-
+      <Bloglist blogs={blogs} title='All posts' />
     </div>
   );
 }
